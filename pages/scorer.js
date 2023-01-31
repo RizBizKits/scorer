cbfb3893378effda3c93525d5509ea8ebe6fc023
Riz <rizwana.akmal@hotmail.com>
2023-01-31 12:19:22 +0000

    clean

--- pages/scorer.js
+++ pages/scorer.js
@@ -1,15 +1,20 @@
 export default function Score() {
   return (
     <div className="h-screen">
-      <p className="text-2xl my-5 max-w-screen-xl mx-2">Score101</p>
+      <p className="text-2xl my-4 max-w-screen-xl mx-2">Score101</p>
       <div className="results-wrapper max-w-screen-xl mx-auto">
         <div className="over-wrapper">
-          <p className="text-xl my-2 mx-2">Innings</p>
+          <p className="text-xl my-2 mx-2">Cavaliers & Carrington CC Innings</p>
+          <div className="flex flex-row place-content-between mx-2">
+            <p className="ball-count">Over 1 of 8</p>
+            <p className="ball-count">Total: 230</p>
+          </div>
           <div className="ball-wrapper m-2  border-solid border-2 border-black rounded-lg px-2 py-2">
             <div className="flex flex-row place-content-between">
               <p className="ball-count">1.0</p>
               <p className="ball-count">OUT → Catch</p>
             </div>
+            <p className="text-right">Catch by Shazia, Ball by Samiah</p>
           </div>
           <div className="ball-wrapper m-2  border-solid border-2 border-black rounded-lg px-2 py-2">
             <div className="ball-wrapper-cell grid grid-cols-3 gap-1">
@@ -74,12 +79,14 @@ export default function Score() {
             </div>
           </div>
         </div>
-        <p className="text-center text-md">Over 1 of 8</p>
+        {/* <p className="text-center text-md">Over 1 of 8</p> */}
+        <p className="text-center text-md">Batting Pair 1 of 4</p>
+
         <div className="flex flex-row place-content-center gap-3">
           <p className="text-center text-md">This Over: 10</p>
           <p className="text-center text-md">This Pair: 17</p>
         </div>
-        <p className="text-center text-xl">Total: 230</p>
+        {/* <p className="text-center text-xl">Total: 230</p> */}
       </div>
     </div>
   );
