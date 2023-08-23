11dd9448d66f3934766b4ced222b8dcc87be521a
Riz <rizwana.akmal@hotmail.com>
2023-08-23 01:57:15 +0100

    add analytics

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,13 @@
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({ children }) {
   return (
     <html className="m-0 p-0">
       <head />
-      <body className="h-screen w-screen">{children}</body>
+      <body className="h-screen w-screen">
+        {children} <Analytics />
+      </body>
     </html>
   );
 }
